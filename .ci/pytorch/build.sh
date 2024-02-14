1c1f8a168d8ff2cd2ff7f5b79930c33fdeff24bb
DanilBaibak <baibak@meta.com>
2024-02-14 15:57:11 +0100

    Trigger the workflow

--- .ci/pytorch/build.sh
+++ .ci/pytorch/build.sh
@@ -247,7 +247,7 @@ else
     # or building non-XLA tests.
     if [[ "$BUILD_ENVIRONMENT" != *rocm*  &&
           "$BUILD_ENVIRONMENT" != *xla* ]]; then
-      WERROR=1 sudo python setup.py bdist_wheel
+      WERROR=1 python setup.py bdist_wheel
     else
       python setup.py bdist_wheel
     fi
