c995b0abd18388af6a737c3d51bb4aaa9cca1789
Jiong Gong <jiong.gong@intel.com>
2024-02-15 10:11:19 +0800

    [inductor][cpp] generalize vector mask for dtypes
    ghstack-source-id: 49d8eb3a0bd247604b38838c3fc13f85febe51b4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119654

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -35,7 +35,6 @@ from ..utils import (
     free_symbol_startswith,
     IndentedBuffer,
     sympy_dot,
-    sympy_index_symbol,
     sympy_subs,
     unique,
 )
@@ -1499,7 +1498,7 @@ class Kernel(CodeGen):
                         )
 
                     self.indirect_max_sizes[map_key] = (size, self.index_to_str(size))
-                return sympy_index_symbol(str(var))
+                return parent_handler.indirect_indexing(var, size, check)
 
             @staticmethod
             def load(name: str, index: sympy.Expr) -> CSEVariable:
